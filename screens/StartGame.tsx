1032e9a8983c58554e328f3daa137e90392b1ecd
Facundo <facuscardillo@gmail.com>
2023-09-02 21:31:17 -0300

    Add styles for the main container

--- screens/StartGame.tsx
+++ screens/StartGame.tsx
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGame: React.FC = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>
         Reset
@@ -16,3 +16,22 @@ const StartGame: React.FC = () => {
 }
 
 export default StartGame;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+})

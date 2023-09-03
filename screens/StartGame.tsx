2bf4de99cde4e1e4d3c57b82cd13c86588fb975f
Facundo <facuscardillo@gmail.com>
2023-09-02 21:19:34 -0300

    Create first files

---
+++ screens/StartGame.tsx
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+const StartGame: React.FC = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  )
+}
+
+export default StartGame;

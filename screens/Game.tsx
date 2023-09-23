d7d32da93e2823672f7b44bc3bd54ea662c79158
Facundo <facuscardillo@gmail.com>
2023-09-23 20:16:02 -0300

    First steps with game logic

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -1,8 +1,9 @@
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, Alert } from 'react-native';
 
 import Title from '../components/ui/Title';
 import { useState } from 'react';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 const generateRandomNumber = (min: number, max: number, exclude: number): number => {
   const randomNumber = Math.floor(Math.random() * (max - min) + min);
@@ -16,8 +17,26 @@ interface GameProps {
   userNumber: number;
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const Game: React.FC<GameProps> = ({ userNumber }) => {
-  const [guess, setGuess] = useState<number>(generateRandomNumber(1, 100, userNumber));
+  const [guess, setGuess] = useState<number>(generateRandomNumber(minBoundary, maxBoundary, userNumber));
+
+  const nextGuessHandler = (direction: 'greater' | 'lower') => {
+    if (direction === 'lower' && guess < userNumber || direction === 'greater' && guess > userNumber) {
+      Alert.alert("Don't lie!", 'You know that this is wrong', [{ text: 'Sorry!', style: 'cancel' }])
+      return;
+    }
+    if (direction === 'lower') {
+      maxBoundary = guess;
+    } else {
+      minBoundary = guess + 1;
+    }
+    const newGuess = generateRandomNumber(minBoundary, maxBoundary, guess);
+    setGuess(newGuess);
+  }
+
   return (
     <View style={styles.screenContainer}>
       <Title>
@@ -26,6 +45,16 @@ const Game: React.FC<GameProps> = ({ userNumber }) => {
       <NumberContainer>
         {userNumber}
       </NumberContainer>
+      <View>
+        <Text>
+          Higher or lower?
+        </Text>
+        <View>
+          <PrimaryButton onPress={() => nextGuessHandler('greater')}>+</PrimaryButton>
+          <PrimaryButton onPress={() => nextGuessHandler('lower')}>-</PrimaryButton>
+        </View>
+        <Text>{guess}</Text>
+      </View>
     </View>
   )
 }

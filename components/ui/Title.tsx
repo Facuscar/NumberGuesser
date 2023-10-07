7dd01e045f7ae7a14cb16f0d2689f71011e0af27
Facundo <facuscardillo@gmail.com>
2023-10-07 19:31:53 -0300

    fix border color

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     borderWidth: 2,
-    borderColor: 'white ',
+    borderColor: 'white',
     padding: 12,
   },
 })

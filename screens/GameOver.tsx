6c9eeea476629410cefeaf9fc3991a4c7016543f
Facundo <facuscardillo@gmail.com>
2023-12-10 20:05:53 -0300

    More improvements for responsiveness

--- screens/GameOver.tsx
+++ screens/GameOver.tsx
@@ -1,4 +1,4 @@
-import { View, Image, StyleSheet, Text, Dimensions } from "react-native";
+import { View, Image, StyleSheet, Text, Dimensions, useWindowDimensions, ScrollView } from "react-native";
 import Title from "../components/ui/Title";
 import { Colors } from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
@@ -9,26 +9,48 @@ interface GameOverProps {
   rounds: number;
 }
 
-const GameOver: React.FC<GameOverProps> = ({userNumber, rounds, onStartNewGame}) => (
-  <View style={styles.rootContainer}>
-    <Title>
-      Game over!
-    </Title>
-    <View style={styles.imageContainer}>
-      <Image style={styles.image} source={require('../assets/images/success.png')} />
-    </View>
-    <Text style={styles.summaryText}>
-      Your phone took <Text style={styles.highLight}>{rounds}</Text> guesses to find the number <Text style={styles.highLight}>{userNumber}</Text>
-    </Text>
-    <PrimaryButton onPress={onStartNewGame}>Start a new game</PrimaryButton>
-  </View>
-)
+const GameOver: React.FC<GameOverProps> = ({userNumber, rounds, onStartNewGame}) => {
+  const { width, height } = useWindowDimensions();
+
+  const getImageSize = () => {
+    if (height < 400) return 80;
+    if (width < 300) return 150;
+    return 300;
+  }
+
+  const imageStyle = {
+    width: getImageSize(),
+    height: getImageSize(),
+    borderRadius: getImageSize() / 2,
+
+  }
+
+  return (
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>
+          Game over!
+        </Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image style={styles.image} source={require('../assets/images/success.png')} />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone took <Text style={styles.highLight}>{rounds}</Text> guesses to find the number <Text style={styles.highLight}>{userNumber}</Text>
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start a new game</PrimaryButton>
+      </View>
+    </ScrollView>
+  )
+};
 
 export default GameOver;
 
 const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -36,9 +58,6 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   imageContainer: {
-    borderRadius: deviceWidth < 380 ? 75 : 150,
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
     borderWidth: 3,
     borderColor: Colors.PRIMARY_800,
     overflow: 'hidden',
